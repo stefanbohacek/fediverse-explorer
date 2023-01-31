a96c6c85bc4101f48f3eb78c7883ad78fc8afc02
Stefan Bohacek <stefan@fourtonfish.com>
2023-01-30 22:06:13 -0500

    Fixed request handling.

--- routes/search.js
+++ routes/search.js
@@ -1,10 +1,19 @@
 import express  from 'express';
 import * as cheerio from 'cheerio';
 
-import Parser from 'rss-parser';
-const parser = new Parser();
-
 const router = express.Router();
+
+const search = async (instance, tag) => {
+    let results = [];
+    try {
+        const resp = await fetch(`${instance}/tags/${tag}.rss`);
+        const xml = await resp.text();
+        return xml;
+    } catch (error) {
+        console.log(error);
+        return results;
+    }
+}
 
 router.get('/', (req, res) => {
     let response = {};
@@ -19,28 +28,17 @@ router.get('/', (req, res) => {
             instances = ['https://mastodon.social', 'https://pawoo.net', 'https://mstdn.jp', 'https://mastodon.cloud'];
         }
 
-        console.log({tag: req.query.tag, instances});
+        // console.log({tag: req.query.tag, instances});
         
         const requests = [];
         instances.forEach(instance => {
-            let feedItems = [];
-            console.log(`${instance}/tags/${req.query.tag}.rss`);
+            // console.log(`${instance}/tags/${req.query.tag}.rss`);
             requests.push(
                 new Promise((resolve, reject) => {
                     (async () => {
-                        const search = async (instance, tag) => {
-                            let results = [];
-                            try {
-                                const resp = await fetch(`${instance}/tags/${tag}.rss`);
-                                const xml = await resp.text();
-                                return xml;
-                            } catch (error) {
-                                console.log(error);
-                                return results;
-                            }
-                        }
+                        let feedItems = [];
                         
-                        search(instance, req.query.tag).then(xml => {
+                        await search(instance, req.query.tag).then(xml => {
                             const $ = cheerio.load(xml, {
                                 xmlMode: true
                             });
@@ -62,20 +60,16 @@ router.get('/', (req, res) => {
                                     item['media_description'] = $media.find('media\\:description').text();
                                 }
                                 feedItems.push(item);
-                                console.log(item);
+                                // console.log(item);
                             });
                         });
-                        let feed = await parser.parseURL(`${instance}/tags/${req.query.tag}.rss`);
                         const response = {}
                         // console.log(feed);
-
-                        
                         response[instance] = feedItems;
                         resolve(response);
                     })();
                 })
             );
-
         });
 
         Promise.all(requests).then((data) => {
