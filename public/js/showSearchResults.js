1b3f5baca86c8be95c9d39d5f74952b66936714f
Stefan Bohacek <stefan@stefanbohacek.com>
2023-12-21 14:29:34 -0500

    Don't link to account page if username is not known.

--- public/js/showSearchResults.js
+++ public/js/showSearchResults.js
@@ -1,94 +1,122 @@
-import toggleFormFields from './toggleFormFields.js';
-import updateMasonryLayout from './updateMasonryLayout.js';
-import timeSince from './timeSince.js';
-import search from './search.js';
+import toggleFormFields from "./toggleFormFields.js";
+import updateMasonryLayout from "./updateMasonryLayout.js";
+import timeSince from "./timeSince.js";
+import search from "./search.js";
 
 const showSearchResults = (tagBrowserForm) => {
-    const tagField = document.getElementById('tag');
-    const instanceList = document.getElementById('instances');
-    const userInstanceField = document.getElementById('user-instance');
-    const resultsContainer = document.getElementById('results');
-    const placeholders = document.getElementById('placeholders');
+  const tagField = document.getElementById("tag");
+  const instanceList = document.getElementById("instances");
+  const userInstanceField = document.getElementById("user-instance");
+  const resultsContainer = document.getElementById("results");
+  const placeholders = document.getElementById("placeholders");
 
-    toggleFormFields(tagBrowserForm, false);
+  toggleFormFields(tagBrowserForm, false);
 
-    const tag = tagField.value.trim();
-    let userInstance = userInstanceField.value.trim();
+  const tag = tagField.value.trim();
+  let userInstance = userInstanceField.value.trim();
 
-    if (userInstance && !userInstance.includes('http')){
-        userInstance = `https://${userInstance}`;
-    }
+  if (userInstance && !userInstance.includes("http")) {
+    userInstance = `https://${userInstance}`;
+  }
 
-    if (tag){
-        // console.log(instanceList.value.split('\n'));
-        placeholders.classList.remove('d-none');
+  if (tag) {
+    // console.log(instanceList.value.split('\n'));
+    placeholders.classList.remove("d-none");
 
-        search({
-            tag: tag,
-            instances: instanceList.value.trim().split('\n') || instanceList.placeholder.split(',')
-        }, resultsContainer).then(results => {
-            placeholders.classList.add('d-none');
+    search(
+      {
+        tag: tag,
+        instances:
+          instanceList.value.trim().split("\n") ||
+          instanceList.placeholder.split(","),
+      },
+      resultsContainer
+    ).then((results) => {
+      placeholders.classList.add("d-none");
 
-            // console.log(results);
-            if (results && results.length){
-                let resultsHTML = '<div class="row">';
-            
-                results.forEach(post => {
-                    const domain = (new URL(post.guid));
-                    const postLink = ( userInstance ? `${userInstance}/authorize_interaction?uri=${encodeURI(post.link)}` : post.link);
+      // console.log(results);
+      if (results && results.length) {
+        let resultsHTML = '<div class="row">';
 
-                    let attachmentHTML = '';
+        results.forEach((post) => {
+          const domain = new URL(post.guid);
+          const postLink = userInstance
+            ? `${userInstance}/authorize_interaction?uri=${encodeURI(
+                post.link
+              )}`
+            : post.link;
 
-                    if (post.media_url){
-                        switch (post.media_type) {
-                            case 'image':
-                                attachmentHTML = `<a href="${postLink}" target="_blank">
+          let attachmentHTML = "";
+
+          if (post.media_url) {
+            switch (post.media_type) {
+              case "image":
+                attachmentHTML = `<a href="${postLink}" target="_blank">
                                 <img onload="updateMasonryLayout()" src="${post.media_url}" class="w-100" alt="">
                                 </a>`;
-                                break;
-                            case 'audio':
-                                attachmentHTML = `<audio class="w-100 p-1 mt-2" onloadeddata="updateMasonryLayout()" controls><source src="${post.media_url}" type="audio/mp3"></audio>`;
-                                break;
-                            default:
-                                attachmentHTML = `<video onloadeddata="updateMasonryLayout()" controls><source src="${post.media_url}" type="video/mp4"></video>`;
-                                break;
-                        }
-                    }
+                break;
+              case "audio":
+                attachmentHTML = `<audio class="w-100 p-1 mt-2" onloadeddata="updateMasonryLayout()" controls><source src="${post.media_url}" type="audio/mp3"></audio>`;
+                break;
+              default:
+                attachmentHTML = `<video onloadeddata="updateMasonryLayout()" controls><source src="${post.media_url}" type="video/mp4"></video>`;
+                break;
+            }
+          }
 
-                    // post.content
-                    // post.contentSnippet
+          // post.content
+          // post.contentSnippet
 
-                    console.log('post.guid', post.guid);
+          console.log("post.guid", post.guid);
 
-                    let username;
+          // let username = "???";
+          let username = "unknown account";
+          let usernameKnown = false;
 
-                    if (post.guid.includes('@')){
-                        username = domain.pathname.split('/')[1];
+          if (post.guid.includes("@")) {
+            username = domain.pathname.split("/")[1];
+            usernameKnown = true;
+          }
 
-                    } else {
-                        username = 'account';
-                    }
+          let postContent = post.content
+            .replaceAll("<a ", '<a target="_blank" ')
+            .replaceAll('class="invisible"', 'class="d-none"');
 
-                    let postContent = post.content.replaceAll('<a ', '<a target="_blank" ')
-                                                  .replaceAll('class="invisible"', 'class="d-none"');
+          //TODO: Quick workaround for CWs.
 
-                    //TODO: Quick workaround for CWs.
+          if (postContent.includes("<strong>Content warning:</strong>")) {
+            postContent = postContent
+              .replace(
+                /* html */ `<p><strong>Content warning:</strong>`,
+                /* html */ `<details onclick="updateMasonryLayout(1)"><summary>`
+              )
+              .replace(/* html */ `</p><hr /><p>`, /* html */ `</summary><p>`);
+            postContent += /* html */ `</details>`;
+          }
 
-                    if (postContent.includes('<strong>Content warning:</strong>')){
-                        postContent = postContent.replace('<p><strong>Content warning:</strong>', '<details onclick="updateMasonryLayout(1)"><summary>')
-                                                 .replace('</p><hr /><p>', '</summary><p>');
-                        postContent += '</details>';
-                    }
-
-                    resultsHTML += `
+          resultsHTML += /* html */ `
                     <div class="col-sm-6 col-lg-4 mt-3 mb-3" data-user="${username}@${domain.host}">
                         <div class="card shadow-sm">
-                            <div class="card-header">
-                            <a href="https://${domain.host}/${username}" class="link-dark fw-bold text-muted text-decoration-none" target="_blank">
-                                <h5 class="card-title">${username}</h5>
-                                <h6 class="card-subtitle mb-2 text-muted">@${domain.host}</h6>
-                            </a>
-                            </div>                                      
+                            <div class="card-header">`;
+
+          if (usernameKnown) {
+            resultsHTML += /* html */ `
+            <a href="https://${domain.host}/${username}" class="link-dark fw-bold text-muted text-decoration-none" target="_blank">
+            <h5 class="card-title">${username}</h5>
+            `;
+          } else {
+            resultsHTML += /* html */ `
+            <h5 class="card-title text-muted">${username}</h5>
+            `;
+          }
+
+          resultsHTML += /* html */ `<h6 class="card-subtitle mb-2 text-muted">@${domain.host}</h6>`;
+
+          if (usernameKnown) {
+            resultsHTML += /* html */ `</a>`;
+          }
+
+          resultsHTML += /* html */ `</div>                                      
                             ${attachmentHTML}
                             <div class="card-body">
                                 <div class="card-text">
@@ -96,32 +124,30 @@ const showSearchResults = (tagBrowserForm) => {
                                 </div>
                             </div>
                             <div class="card-footer text-muted">
-                                <a class="link-dark fw-bold text-muted text-decoration-none card-link" href="${postLink}" target="_blank">${timeSince(new Date(post.pubDate))} ago</a>
+                                <a class="link-dark fw-bold text-muted text-decoration-none card-link" href="${postLink}" target="_blank">${timeSince(
+            new Date(post.pubDate)
+          )} ago</a>
                             </div>
                         </div>
                     </div>
                     `;
-                                                
-                });
-                
-                resultsHTML += `
+        });
+
+        resultsHTML += /* html */ `
                 </div>
                 `;
 
-                resultsContainer.innerHTML = resultsHTML;
-                toggleFormFields(tagBrowserForm, true);
-                updateMasonryLayout();                    
-            } else {
-                alert('Nothing found.');
-                toggleFormFields(tagBrowserForm, true);
-            }         
-        });
-    } else {
-        alert('Please enter at least one tag.');
-    }
-}
+        resultsContainer.innerHTML = resultsHTML;
+        toggleFormFields(tagBrowserForm, true);
+        updateMasonryLayout();
+      } else {
+        alert("Nothing found.");
+        toggleFormFields(tagBrowserForm, true);
+      }
+    });
+  } else {
+    alert("Please enter at least one tag.");
+  }
+};
 
 export default showSearchResults;
-
-
-
